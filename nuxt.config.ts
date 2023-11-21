d916f32fe2c0326c48cdd9041d484aa92ff81368
detailcore <savemysoulok@yandex.ru>
2023-11-21 10:45:44 +0400

    Add autoSort tailwind class & Fonts & Colors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  ssr: false,
 
-  // css: ['~/assets/styles/main.css'], // global styles
+  telemetry: false,
 
-  modules: ['@nuxtjs/eslint-module', 'nuxt-purgecss', '@nuxtjs/tailwindcss', 'nuxt-vitest'],
+  devtools: {
+    enabled: false,
+  },
+
+  // css: ['~/assets/styles/base.css'], // global styles
+
+  modules: ['@nuxtjs/eslint-module', 'nuxt-purgecss', '@nuxtjs/tailwindcss', 'nuxt-vitest', '@nuxtjs/google-fonts'],
 
   purgecss: {
     enabled: false, // Always enable purgecss
     // safelist: [''], // Add my-class token to the safelist (e.g. .my-class)
+  },
+
+  // https://google-fonts.nuxtjs.org/getting-started
+  googleFonts: {
+    families: {
+      Heebo: [400, 500, 700, 900],
+    },
+    display: 'swap',
   },
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,36 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [],
+  // content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './src/components/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heebo: ['Heebo', 'Open Sans', ...defaultTheme.fontFamily.sans],
+      },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      primary: '#FF6464',
+      secondry: '#00A8CC',
+      dark: '#21243D',
+      light: '#8695A4',
+      gray: {
+        50: '#f9fafb',
+        100: 'f3f4f6',
+        200: 'e5e7eb',
+        300: '#d1d5db',
+        400: '#9ca3af',
+        500: '#6b7280',
+        600: '#4b5563',
+        700: '#374151',
+        800: '#1f2937',
+        900: '#111827',
+        950: '#030712',
+      },
+    },
   },
   plugins: [],
 }
