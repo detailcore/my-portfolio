ec81eb8cf640959a79f94fa1a5fa399f8c455bc8
savemysoulok@yandex.ru <admin@server.example.com>
2023-12-05 13:41:20 +0300

    pm2 change setting

--- ecosystem.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "Portfolio",
-  port: 3005,
-  // exec_mode: "cluster",
-  // instances: 3, // 'max' or a number of instances
-  script: "./.output/server/index.mjs",
-  args: "start",
-};
