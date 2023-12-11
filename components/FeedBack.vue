3bdf0eef737fe6db591c0c3a0df3f6ec0181aa06
detailcore <savemysoulok@yandex.ru>
2023-12-11 16:44:58 +0400

    change footer

---
+++ components/FeedBack.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="mb-7 flex h-[30px] w-full justify-center">
+    <NuxtLink
+      target="_blank"
+      to="https://t.me/detailcore"
+      class="mr-9 hover:text-primary focus:text-primary"
+    >
+      <IconsTelegram class="h-10 w-10 max-w-[35px]" />
+    </NuxtLink>
+
+    <NuxtLink
+      target="_blank"
+      to="https://github.com/detailcore"
+      class="hover:text-primary focus:text-primary"
+    >
+      <IconsGithub class="h-10 w-10" />
+    </NuxtLink>
+  </div>
+</template>
--- components/RegionFooter.vue
+++ components/RegionFooter.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+const route = useRoute()
+
 const year = computed<number>(() => {
   return new Date().getFullYear()
 })
@@ -6,33 +8,19 @@ const year = computed<number>(() => {
 
 <template>
   <footer class="mt-12 flex h-44 w-full flex-col items-center justify-center">
-    <div class="mb-7 flex h-[30px] w-full justify-center">
-      <NuxtLink
-        target="_blank"
-        to="https://t.me/detailcore"
-        class="mr-9 hover:text-primary focus:text-primary"
-      >
-        <IconsTelegram class="h-10 w-10 max-w-[35px]" />
-      </NuxtLink>
+    <FeedBack />
 
-      <NuxtLink
-        target="_blank"
-        to="https://github.com/detailcore"
-        class="hover:text-primary focus:text-primary"
-      >
-        <IconsGithub class="h-10 w-10" />
-      </NuxtLink>
-    </div>
-
-    <div>Copyright ©{{ year }} All rights reserved.</div>
-    <div class="mt-2 flex text-sm">
-      Figma template (<NuxtLink
-        target="_blank"
-        to="https://www.figma.com/file/SLHsCYqjbiMLZ18sYcuM3B"
-        class="hover:text-primary focus:text-primary"
-      >
-        link </NuxtLink
-      >)
+    <div v-show="route.name !== 'contact'" class="flex flex-col items-center">
+      <div>Copyright ©{{ year }} All rights reserved.</div>
+      <div class="mt-2 flex text-sm">
+        Figma template (<NuxtLink
+          target="_blank"
+          to="https://www.figma.com/file/SLHsCYqjbiMLZ18sYcuM3B"
+          class="hover:text-primary focus:text-primary"
+        >
+          link </NuxtLink
+        >)
+      </div>
     </div>
   </footer>
 </template>
